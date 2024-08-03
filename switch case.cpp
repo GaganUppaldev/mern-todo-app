75d26b98f7388071a21b2869027bd2a8aa130b55
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-03 21:35:22 +0530

    Create switch case.cpp
    here I added switch case basic example and soon i will add calculator code in comment tag

---
+++ switch case.cpp
@@ -0,0 +1,32 @@
+//switch case 
+
+#include <iostream>
+using namespace std;
+int main(){
+  int num = 10;
+  int test = 40;
+
+  switch(num){
+    case 1 : cout<<"hello"<<endl;
+             break;
+    case 10: cout<<"welcome sir"<<endl;
+             break;
+    case 3 : cout<<"wOw"<<endl;
+             break;
+    
+  }
+
+  switch(test){
+    case 10 : cout<<"10"<<endl;
+             break;
+    case 40: cout<<"40"<<endl;
+             break;
+    case 30 : cout<<"30"<<endl;
+             break;
+
+  }
+
+  
+}
+
+//continue is not valiad in switch case
