0a8d47b3668e4ab3a16be0e5b6dc4c70fe1f8b65
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-23 01:22:33 +0530

    Update server.js
    Code added to receive post request and than print messages in console of server

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 //here will be server side code using express.js
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 7000;
+
+app.use(cors()); // Use cors middleware
+
+app.use(express.json()); // For parsing application/json
+
+app.post('/print', (req, res) => {
+    console.log('POST request received!');
+    console.log('Message:', req.body.message);
+    res.send('Message received! Check your console!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
