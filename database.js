123df7be68781cd940a49e2e4c4f8d8918cd0b72
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-03 22:06:54 +0530

    Update database.js
    How one error can eat whole day

--- database.js
+++ database.js
@@ -1,1 +1,4 @@
-
+/*I have spended whole from Morning to End in solvind one error named( ENCONNECTION refused ) it came in both mongoDB and than postsql actually
+trying to create API and only Database was left so i choose mongo but i caused error i tried hard but failed to solve than i moved to postgresql 
+, installed it  created full setput , connected it , install package named pg but at the end again same error , checked fire fall so This is 
+problem which need to be noted as in starting very frustating  can't solved after spending whole day*/
