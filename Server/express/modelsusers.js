acfbc2efd178bcedc8280ccb424ef0921ffbe977
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-11 13:55:02 +0530

    Update modelsusers.js
    added tasks in user schema and rest about to-do schema i need to check and will modified soon if required

--- Server/express/modelsusers.js
+++ Server/express/modelsusers.js
@@ -1,19 +1,44 @@
 import mongoose from 'mongoose';
 
+// Define the schema for a to-do item
+const todoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
 // Define the schema for the `users` collection
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+  name: {
+      type: String,
+      required: true,
+  },
+  password: {
+      type: String,
+      required: true,
+  },
+  tasks: [
+      {
+          text: String,
+          isEditing: Boolean,
+          done: Boolean
+      }
+  ]
 });
+
 
 // Create the model for the `users` collection
 const User = mongoose.model('User', userSchema, 'users');
 
 // Export the model
 export default User;
+
